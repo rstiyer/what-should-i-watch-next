4df8012722f6e51c4c3b926b22fa6ea9a2677fad
rstiyer <rachel.stiyer@gmail.com>
2016-08-03 16:17:24 -0500

    implement ajax calls to TMDB api for movie data and display results

--- js/app.js
+++ js/app.js
@@ -5,20 +5,57 @@ $(function() {
 		e.preventDefault();
 		var userInput = $('input[name="title"]').val();
 		console.log(userInput);
-		getMovie(userInput);
+		getMovieRecommendations(userInput);
 	});
 });
 
-function getMovie(userInput) {
+function getMovieRecommendations(userInput) {
 	$.ajax({
 		url: "http://api.themoviedb.org/3/search/movie",
 		method: "GET",
 		data: {
 			query: userInput,
 			api_key: devKey
-		}
+		},
+		dataType: "jsonp"
 	})
 	.done(function(response) {
-		console.log(response);
+		var inputId = response.results[0].id;
+		console.log(inputId);
+		var baseUrl = "http://api.themoviedb.org/3/movie/" + inputId + "/similar";
+
+		$.ajax({
+			url: baseUrl,
+			method: "GET",
+			data: {
+				api_key: devKey
+			},
+			dataType: "jsonp"
+		})
+		.done(function(response) {
+			console.log(response);
+			showMovieRecommendations(response.results);
+		})
+	})
+	.fail(function() {
+		// Return error text
+	});
+}
+
+function showMovieRecommendations(results) {
+	$.each(results, function(index, result) {
+		var overviewElt = $('.templates .overview').clone();
+		var detailsElt = $('.templates .details').clone();
+
+		overviewElt.find(".title").text(result.title);
+		overviewElt.find(".poster").attr("src", "https://image.tmdb.org/t/p/w160"+result.poster_path);
+		overviewElt.find(".rating").text(result.vote_average);
+
+		console.log(result.overview);
+		detailsElt.find(".desc").text(result.overview);
+		detailsElt.find(".release-date").text(result.release_date);
+
+		overviewElt.appendTo(".results");
+		detailsElt.appendTo(".results");
 	});
 }
